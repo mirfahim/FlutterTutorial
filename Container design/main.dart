3ebe51e305bcf781157eff4b012ab4cbd59a1c8c
mirfahim <54933139+mirfahim@users.noreply.github.com>
2020-02-19 11:35:57 +0600

    Create main.dart

---
+++ Container design/main.dart
@@ -0,0 +1,13 @@
+new Container(
+            margin: const EdgeInsets.symmetric(vertical: 4.0),
+            decoration: new BoxDecoration(
+              border: new Border(
+                left: new BorderSide(
+                  width: 4.0,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            
+            //  Container take a decoration field. 
+            // by this decoration we can get a blue box to left side of container.
